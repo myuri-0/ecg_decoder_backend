a6e92efc1be72d4e0085ae6837acaa3aa17e24ca
ilya <ilya.05@mail.ru>
2025-11-27 13:29:49 +0300

    настройка бд

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response, Depends
 import uvicorn
+from authx import AuthX, AuthXConfig
+from sqlalchemy import select
+from dotenv import dotenv_values
+
+from postgres_db import get_session
+from models import User
+from schemas import UserLoginSchema
+
 
 app = FastAPI()
+tokens_and_keys = dotenv_values(".env")
 
-@app.get("/login")
-def login():
-    pass
+config = AuthXConfig()
+config.JWT_SECRET_KEY = tokens_and_keys['SECRET_KEY']
+config.JWT_ACCESS_COOKIE_NAME = "my_access_token"
+config.JWT_TOKEN_LOCATION = ["cookies"]
 
+security = AuthX(config=config)
+
+
+@app.post("/login")
+async def login(
+    creds: UserLoginSchema,
+    response: Response,
+    session=Depends(get_session)
+):
+    query = select(User).where(User.email == creds.email)
+    result = await session.execute(query)
+    user = result.scalar()
+    if not user:
+        raise HTTPException(status_code=401, detail="incorrect username or password")
+    if creds.password != user.password:
+        raise HTTPException(status_code=401, detail="incorrect username or password")
+    token = security.create_access_token(uid=str(user.id))
+    response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+
+    return {"access_token": token}
+
+@app.post("/protected", dependencies=[Depends(security.access_token_required)])
+def protected():
+    ...
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
---
+++ postgres_db.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import dotenv_values
+
+env_variable = dotenv_values(".env")
+
+
+DATABASE_URL = env_variable["DATABASE_URL"]
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+
+async_session_maker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+Base = declarative_base()
+
+
+async def get_session():
+    async with async_session_maker() as session:
+        yield session
+
